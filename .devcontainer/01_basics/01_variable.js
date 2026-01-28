622e50b75efd3ea3e4c9e38a43f2af7ddcfa29e7
Poulami Sarkar <poulamisarkar2004@gmail.com>
2026-01-28 13:49:12 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144543
+let accountEmail ="poulami@google.com"
+var accountPassword ="12345"
+accountCity ="Kolkata"
+
+//accountId = 2 // not allow
+accountEmail ="polu@gg.com"
+accountPassword="21546"
+accountCity="mumbai"
+
+console.log(accountId);
+console.table([accountId ,accountEmail ,accountPassword, accountCity]);
+/*
+preffer not to use var because of issue in block scope and funtional scope
+*/
