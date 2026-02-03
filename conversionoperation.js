22b7d6d9e072e9a46d7e6bb26282de9c310a4a11
Poulami Sarkar <poulamisarkar2004@gmail.com>
2026-02-03 08:24:34 +0000

    operations also

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,17 +1,17 @@
 let score = 33;
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let score1 ="33";
-console.log(typeof score1);
+//console.log(typeof score1);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 let score2 = undefined
-console.log(typeof score2);
+//console.log(typeof score2);
 let valueInNumber1 = Number(score2)
-console.log(valueInNumber1);
+//console.log(valueInNumber1);
 
 // 33 => 33
 // 33abc => NaN (not a number)
@@ -20,7 +20,7 @@ console.log(valueInNumber1);
 let isloggedIn = 1
 
 let booleanIsLoggedIn= Boolean(isloggedIn);
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1=> true
 // 0 => false
@@ -29,5 +29,44 @@ console.log(booleanIsLoggedIn)
 
 let someNumber =33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+//**********Operation********
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" Poulami"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2+ "2");
+//console.log(true);
+//console.log(+true);
+//console.log("");
+//console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+//link to study https://tc39.es/ecma262/#sec-global-object//
+
