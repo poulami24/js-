0c674e8421f3b86bf068f969ef7b819f29e300da
Poulami Sarkar <poulamisarkar2004@gmail.com>
2026-02-03 07:13:04 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,14 @@
+"use strict";//treat as js code new version
+//alert(3+3); // we are using node js not brower
+
+let name="Poulami"
+let age =21
+let isloggedIn = true 
+let state;
+
+// number , string "" ,bolean , undefined, null= standalone value,symbol,object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof "Poulami");
+console.log(typeof undefined);
